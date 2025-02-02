8677f552b0e36ebe15e947c0b3f96a8fc46a0d6a
Vera Fesianava <verafes@gmail.com>
2025-02-02 12:24:59 -0800

    Add templates, test data,  utils preconditions

---
+++ tests/search.func.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from "@playwright/test"
+
+test.describe('Should Search Users by Search Criteria', async() => {
+
+    test.beforeEach('Before Each Name', async({ page }) => {
+
+    })
+
+    test('Search User with Unique First Name', async({ page }) => {
+
+    })
+
+    test('Search User with Non-unique First Name', async({ page }) => {
+
+    })
+})
---
+++ tests/template.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('Tests Suite Name', async() => {
+
+    test.beforeEach('Before Each Name', async({ page }) => {
+
+    })
+
+    test('Unique Test Name', async({ page }) => {
+
+    })
+})
